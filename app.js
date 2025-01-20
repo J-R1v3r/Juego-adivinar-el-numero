0fb2b434f26904b4aace6a7755b3e8f2b0eb8822
Daniel Lagos <daniel.edulagos20@gmail.com>
2025-01-20 13:56:27 -0300

    Subiendo juego

---
+++ app.js
@@ -0,0 +1,86 @@
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
+
+function asignarTextoElemento(elemento, texto) {
+  let elementoHTML = document.querySelector(elemento);
+  elementoHTML.innerHTML = texto;
+  return;
+}
+
+function verificarIntento() {
+  let valorUsuario = document.getElementById("valorUsuario").value;
+
+  // Validar si el usuario ingreso o no un valor
+  if (valorUsuario.trim() === "") {
+    asignarTextoElemento("p", "Debes de ingresar un número para adivinar");
+    return;
+  }
+  // Si el usuario ingreso un valor, convertir el valor en un numero valido
+  let numeroDeUsuario = parseInt(valorUsuario);
+
+  if (numeroDeUsuario === numeroSecreto) {
+    asignarTextoElemento("p", `¡Felicitaciones 🎉 adivinaste el número !`);
+    asignarTextoElemento("span", `Intentos realizados: ${intentos}`);
+
+    document.getElementById("reiniciar").removeAttribute("disabled");
+  } else {
+    //El usuario no acertó.
+    if (numeroDeUsuario > numeroSecreto) {
+      asignarTextoElemento(
+        "p",
+        `El número secreto es menor que: ${numeroDeUsuario}`
+      );
+    } else {
+      asignarTextoElemento(
+        "p",
+        `El número secreto es mayor que: ${numeroDeUsuario}`
+      );
+    }
+    intentos++;
+    limpiarCaja();
+  }
+  return;
+}
+
+function limpiarCaja() {
+  document.querySelector("#valorUsuario").value = "";
+}
+
+function generarNumeroSecreto() {
+  let numeroGenerado = Math.floor(Math.random() * numeroMaximo) + 1;
+
+  //Si ya sorteamos todos los números
+  if (listaNumerosSorteados.length == numeroMaximo) {
+    asignarTextoElemento("p", "Ya se sortearon todos los números posibles");
+  } else {
+    //Si el numero generado está incluido en la lista
+    if (listaNumerosSorteados.includes(numeroGenerado)) {
+      return generarNumeroSecreto();
+    } else {
+      listaNumerosSorteados.push(numeroGenerado);
+      return numeroGenerado;
+    }
+  }
+}
+
+function condicionesIniciales() {
+  numeroSecreto = generarNumeroSecreto();
+  intentos = 1;
+}
+
+function reiniciarJuego() {
+  //limpiar caja
+  limpiarCaja();
+  //Indicar mensaje de intervalo de números
+  //Generar el número aleatorio
+  //Inicializar el número intentos
+  condicionesIniciales();
+  //Deshabilitar el botón de nuevo juego
+  document.querySelector("#reiniciar").setAttribute("disabled", "true");
+  asignarTextoElemento("p", "");
+  asignarTextoElemento("span", "");
+}
+
+condicionesIniciales();
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Adivinar el numero
+
+Este proyecto fue desarrollado como parte del curso ONE (Oracle + AluraLatam), el cual tiene como objetivo crear un juego en el que el usuario debe adivinar un número secreto.
+
+**Mejoras recientes agregadas**:
+
+- Rediseñé la interfaz del juego para una mejor experiencia visual.
+- Modifiqué la forma en que se muestran los mensajes al usuario, mejorando la claridad y la interacción.
+- Implementé una funcionalidad para validar si el usuario ingresó un número válido o no.
